e1a1bce2696c83f9534e468e5812760de09bd315
EricStautmeister <71490551+EricStautmeister@users.noreply.github.com>
2023-01-19 23:07:08 +0100

    database connection works
    Co-authored-by: MerlinMinder <MerlinMinder@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+ini_set('display_errors', 0);
+
+require_once 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+?>
 <!DOCTYPE html>
 <html>
 
@@ -7,18 +16,35 @@
     <title>Real Nutrition</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="./styles/index.css">
+    <link rel="stylesheet" href="./styles/normalise.css">
 </head>
 
 <body>
-    <h1>Real Nutrition</h1>
-    <form action="datahandler.php" method="post">
-        <label for="meal">Meal</label>
-        <input type="text" id="meal" name="meal"><br><br>
-        <label for="calories">Calories</label>
-        <input type="text" id="calories" name="calories"><br><br>
-        <button type="submit">Submit</button> 
-    </form>
-    <script src="" async defer></script>
+    <div class="container">
+        <h1>Real Nutrition</h1>
+        <form action="datahandler.php" method="post">
+            <div class="hwrapper">
+                <section class="colsec">
+                    <label for="meal">Meal</label>
+                    <label for="calories">Calories</label>
+                </section>
+                <section class="colsec">
+                    <input type="text" id="meal" name="meal">
+                    <input type="text" id="calories" name="calories">
+                </section>
+            </div>
+            <button type="submit" class="button">Submit</button>
+        </form>
+    </div>
+    <div>
+        <?php
+        include 'db.php';
+        include 'helper/functions.php';
+        include 'datahandler.php'
+        ?>
+    </div>
+    <!-- <script src="" async defer></script> -->
 </body>
+
 </html>
