5e3616a3a8abc34a585a0241b421b9ff0f2fa215
Eric Stautmeister <71490551+EricStautmeister@users.noreply.github.com>
2023-01-20 11:15:36 +0100

    pages dir

--- db.php
+++ db.php
@@ -10,7 +10,11 @@ $options = array(PDO::MYSQL_ATTR_SSL_CA => $_ENV['MYSQL_ATTR_SSL_CA'],);
 
 try {
     $pdo = new PDO($dsn, $_ENV['USERNAME'], $_ENV['PASSWORD'], $options);
-    echo "Connected";
+    $stm = $pdo->query("SELECT VERSION()");
+
+    $version = $stm->fetch();
+
+    echo $version[0] . PHP_EOL;
 } catch (PDOException $error) {
     echo $error;
 }
