4edd1ccb8046fae11802fed81136fc3a8f83923f
EricStautmeister <71490551+EricStautmeister@users.noreply.github.com>
2023-02-06 17:37:33 +0100

    -

--- model/db.php
+++ model/db.php
@@ -1,24 +1,58 @@
 <?php
 
-function dbVersion($db)
-{
-    try {
-        $stm = $db->query("SELECT VERSION()");
-        $version = $stm->fetch();
+ini_set('display_errors', 0);
+require_once '../vendor/autoload.php';
 
-        return $version[0] . PHP_EOL;
-    } catch (PDOException $error) {
-        return $error;
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+define('EOL', '<br>');
+// include './db_connection.php';
+
+/* DB is a class that connects to a database and 
+has a few methods that allow you to query the database. */
+class DB
+{
+    private $db;
+    function __construct()
+    {
+        try {
+            $dsn = "{$_ENV['DRIVER']}:host={$_ENV['HOST']};dbname={$_ENV['DATABASE']}";
+            $db = new PDO(
+                $dsn,
+                $_ENV['USERNAME'],
+                $_ENV['PASSWORD'],
+                array(
+                    // PDO::MYSQL_ATTR_SSL_KEY    =>'/path/to/client-key.pem',
+                    // PDO::MYSQL_ATTR_SSL_CERT=>'/path/to/client-cert.pem',
+                    // PDO::MYSQL_ATTR_SSL_CA    => $_ENV['MYSQL_ATTR_SSL_CA']
+                )
+            );
+            echo "Connected to the database" . PHP_EOL;
+            $this->db = $db;
+        } catch (PDOException $error) {
+            echo $error;
+        }
+        echo "db class works";
     }
-}
 
-function addUser($db)
-{
-    try {
-        $stm = $db->query("INSERT INTO users (username, password) VALUES ('test', 'test')");
-        
-        return $stm;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-    } catch (PDOException $error) {
-        return $error;
+    public function getUsers()
+    {
+        try {
+            $stm = $this->db->query("SELECT * FROM users");
+
+            return $stm;
+        } catch (PDOException $error) {
+            return $error;
+        }
+    }
+
+    public function addUser($username, $password)
+    {
+        try {
+            return $this->db->query("INSERT INTO users (username, password) VALUES ($username, $password)");
+        } catch (PDOException $error) {
+            return $error;
+        }
     }
 }
--- model/db_connection.php
+++ model/db_connection.php
@@ -1,37 +1,24 @@
 <?php
-ini_set('display_errors', 0);
-require_once '../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->safeLoad();
-
-function dbVersion($db)
+function connectDB()
 {
     try {
-        $stm = $db->query("SELECT VERSION()");
-        $version = $stm->fetch();
+        $dsn = "{$_ENV['DRIVER']}:host={$_ENV['HOST']};dbname={$_ENV['DATABASE']}";
+        $db = new PDO(
+            $dsn,
+            $_ENV['USERNAME'],
+            $_ENV['PASSWORD'],
+            array(
+                // PDO::MYSQL_ATTR_SSL_KEY    =>'/path/to/client-key.pem',
+                // PDO::MYSQL_ATTR_SSL_CERT=>'/path/to/client-cert.pem',
 
-        return $version[0] . PHP_EOL;
+                // PDO::MYSQL_ATTR_SSL_CA    => $_ENV['MYSQL_ATTR_SSL_CA']
+            )
+        );
+        echo "Connected to the database" . PHP_EOL;
+        return $db;
     } catch (PDOException $error) {
-        return $error;
+        echo $error;
+        return null;
     }
-}
-
-try {
-    $dsn = "{$_ENV['DRIVER']}:host={$_ENV['HOST']};dbname={$_ENV['DATABASE']}";
-    $db = new PDO(
-        $dsn,
-        $_ENV['USERNAME'],
-        $_ENV['PASSWORD'],
-        array(
-            // PDO::MYSQL_ATTR_SSL_KEY    =>'/path/to/client-key.pem',
-            // PDO::MYSQL_ATTR_SSL_CERT=>'/path/to/client-cert.pem',
-            
-            // PDO::MYSQL_ATTR_SSL_CA    => $_ENV['MYSQL_ATTR_SSL_CA']
-        )
-    );
-    echo dbVersion($db);
-    echo "Connected to the database" . PHP_EOL;
-} catch (PDOException $error) {
-    echo $error;
 }
