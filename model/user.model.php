16cf2e95a41a286831ef26670fed028344988b07
EricStautmeister <71490551+EricStautmeister@users.noreply.github.com>
2023-03-16 11:38:35 +0100

    Todo's

--- model/user.model.php
+++ model/user.model.php
@@ -1,6 +1,9 @@
 <?php
 require_once "model.php";
 
+// TODO: Rewrite this class to use the new model class.
+// TODO: Add all necessary CRUD methods.
+
 class UserModel extends Model
 {
     public function __construct(string $name)
