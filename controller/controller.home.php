c23365efdd93024761ea8530831424de4321b7fa
MerlinMinder <merlin.coding@gmail.com>
2023-03-17 19:44:45 +0100

    merge commit

--- controller/controller.home.php
+++ controller/controller.home.php
@@ -2,10 +2,32 @@
 
 class HomeController
 {
+    private $model;
+
+    public function __construct()
+    {
+        // $this->model = new FoodModel();
+    }
     public function handleRequest()
     {
-        // sets php variables and displays homepage
-        $title = "brahhello";
-        include "view/home.php";
+        if (empty($_POST)) {
+            $this->displayPage();
+        } else {
+            $this->handlePost();
+        }
+    }
+
+    private function displayPage($args = [])
+    {
+        extract($args);
+        var_dump($args);
+        $foods = array("apple", "banana", "orange", "pear", "cookies", "apple", "banana", "orange", "juice", "cookies", "meats", "sausage", "rice", "pasta", "tomato", "onion", "cucumber", "garlic", "carrot", "carrotpie", "canned tuna", "oil", "peanut", "peanut oil", "peanutbutter");
+        // $foods = $this->model->getFoodNames();
+        include "home.testing.php";
+    }
+
+    private function handlePost()
+    {
+        $this->displayPage($_POST);
     }
 }
