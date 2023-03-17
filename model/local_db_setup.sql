52100663acb230b8dee5e9b9cf791c2b2ce5f45b
EricStautmeister <71490551+EricStautmeister@users.noreply.github.com>
2023-03-17 19:36:11 +0100

    no longer needed, is handled by model

--- model/local_db_setup.sql
+++
@@ -1,6 +0,0 @@
--- CREATE TABLE IF NOT EXISTS `auth` (
---     `id` BIGINT NOT NULL AUTO_INCREMENT,
---     `email` varchar(255) NOT NULL,
---     `password` varchar(255) NOT NULL,
---     PRIMARY KEY (`id`)
--- );
