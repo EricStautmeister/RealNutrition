f087e6327bfc097a27c94a02510167012217a855
EricStautmeister <71490551+EricStautmeister@users.noreply.github.com>
2023-03-18 17:35:40 +0100

    foodmodel interface with all methods implemented

--- model/food.model.php
+++ model/food.model.php
@@ -2,15 +2,52 @@
 
 require_once 'model.php';
 
-class FoodModelWrapper extends ModelFactory
-{
+interface FoodModel {
+    public function getFoodNames(): array;
+    public function getFoodData(string $foodName): array;
+    public function getFoodDataByColumn(string $column, string $data): array;
+    public function addFood(
+        string $name,
+        string $description,
+        string $calories,
+        string $fat = null,
+        string $protein = null,
+        string $carbs = null,
+        string $saturated_fat = null,
+        string $sodium = null,
+        string $sugar = null,
+        string $cholesterol = null,
+        string $fiber = null,
+        string $potassium = null
+    ): void;
+    public function updateFood(
+        string $constrol_column,
+        string $control_value,
+        string $name,
+        string $description,
+        string $calories,
+        string $fat = null,
+        string $protein = null,
+        string $carbs = null,
+        string $saturated_fat = null,
+        string $sodium = null,
+        string $sugar = null,
+        string $cholesterol = null,
+        string $fiber = null,
+        string $potassium = null
+    ): void;
+    public function deleteFood(string $control_column, string $control_value): void;
+}
+/**
+ * Summary of FoodModelWrapper
+ */
+class FoodModelWrapper extends ModelFactory implements FoodModel {
     private $mandatory_table_elements = [];
     private $optional_table_elements = [];
     private $meta_table_elements = [];
     private $nonMetaTable = [];
     public $data = [];
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct("food");
         $this->mandatory_table_elements = [
             "id",
@@ -60,14 +97,29 @@ class FoodModelWrapper extends ModelFactory
         );
     }
 
-    public function loadData()
-    {
+    public function loadData() {
         return $this->selectAll();
     }
 
     // TODO: Add a function to get specific food data, filtered by data and column
 
 
+    /**
+     * Summary of addFood
+     * @param string $name
+     * @param string $description
+     * @param string $calories
+     * @param string|null $fat
+     * @param string|null $protein
+     * @param string|null $carbs
+     * @param string|null $saturated_fat
+     * @param string|null $sodium
+     * @param string|null $sugar
+     * @param string|null $cholesterol
+     * @param string|null $fiber
+     * @param string|null $potassium
+     * @return void
+     */
     public function addFood(
         string $name,
         string $description,
@@ -81,7 +133,7 @@ class FoodModelWrapper extends ModelFactory
         string $cholesterol = null,
         string $fiber = null,
         string $potassium = null,
-    ) {
+    ): void {
         $food_data = [
             $name,
             $description,
@@ -100,31 +152,39 @@ class FoodModelWrapper extends ModelFactory
         ];
         $cols = $this->mandatory_table_elements;
         $cols = array_splice($cols, 1, 3);
-        // echo nl2br("cols: " . print_r($cols, true) . PHP_EOL);
-        // echo nl2br("food data: " . print_r($food_data, true) . "optional food data: " . PHP_EOL . print_r($opt_food_data, true) . PHP_EOL);
         for ($i = 0; $i < count($opt_food_data); $i++) {
-            // echo nl2br("i={$i}: " . $this->optional_table_elements[$i] . $opt_food_data[$i] . PHP_EOL);
             if ($opt_food_data[$i] != null && $opt_food_data[$i] !== "" && $opt_food_data[$i] !== " " && $opt_food_data[$i] !== "0") {
                 array_push($food_data, $opt_food_data[$i]);
                 array_push($cols, $this->optional_table_elements[$i]);
             }
         }
-        $this->insert($cols, $food_data);
-        // for ($i = 0;$i < count($food_data); $i++) {
-        //     echo nl2br("{$cols[$i]}: " . $food_data[$i] . PHP_EOL);
-        // }
-        // echo nl2br("\n\n") . print_r($cols, true) . nl2br("\n");
-        // echo print_r($food_data, true) . nl2br("\n\n");
-
-        // echo nl2br("\n final cols: " . print_r($cols, true) . "\nfinal food data: ||". var_export($food_data) . PHP_EOL);
+        $this->insert($cols, $food_data)->execute();
     }
 
+    public function getFoodNames(): array {
+        $food_names = [];
+        $food_data = $this->selectAll();
+        foreach ($food_data as $food) {
+            array_push($food_names, $food["name"]);
+        }
+        return $food_names;
+    }
+
+    public function getFoodData(string $foodName): array {
+        $food_data = $this->select("name", $foodName);
+        return $food_data;
+    }
+
+    public function getFoodDataByColumn(string $column, string $data): array {
+        $food_data = $this->select($column, $data);
+        return $food_data;
+    }
     // TODO: Test the delete function
     // TODO: Check if there are delete requirements
-    public function deleteFood(string $control_column, string $control_value)
-    {
+    public function deleteFood(string $control_column, string $control_value): void {
         $this->delete($control_column, $control_value);
     }
+
     // TODO: Test the update function
     public function updateFood(
         string $constrol_column,
@@ -141,7 +201,7 @@ class FoodModelWrapper extends ModelFactory
         string $cholesterol = null,
         string $fiber = null,
         string $potassium = null,
-    ) {
+    ): void {
         $food_data = [
             $name,
             $description,
@@ -167,8 +227,7 @@ class FoodModelWrapper extends ModelFactory
         $this->update($constrol_column, $control_value, $cols, $food_data);
     }
 
-    public function save()
-    {
+    public function save(): void {
         foreach ($this->data as $food) {
             $this->insert($food[0], $food[1]);
         }
