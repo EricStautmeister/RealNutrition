3b8425a995a61b5614600d797d9b71ffde773bac
Moreno Valenti <117902309+BiriToc@users.noreply.github.com>
2023-03-21 10:14:02 +0100

    view updated, html in view files

--- view/home.php
+++ view/home.php
@@ -2,6 +2,9 @@
 <html>
 
 <head>
+    <title>
+        Real Nutrition
+    </title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Real Nutrition</title>
@@ -15,8 +18,40 @@
     <header>
         <form action="" method="post"></form>
     </header>
-    Home
+    Home page   
     <script src="" async defer></script>
+    <nav class="navbar">
+        <ul class="list">
+            <li><a href="Placeholder">Dashboard</a></li>
+            <li><a href="Placeholder1">Homepage</a></li>
+            <li><a href="Placeholder2">Guide(?)</a></li>
+        </ul>     
+    </nav>
+    <section class="Intro">
+        <h1 id="RTard">
+            Food Search Engine 
+        </h1>
+        <p1 id="explaintext">
+            Search different Foods to see their different nutritional values
+        </p1>
+
+    </section>
+    <div class="FoodSearchThing">
+        <input type="text" name="FoodSearch" id="FoodSearch" placeholder="Butter">
+        <button class="ClickThingSearch">
+            Search
+        </button>
+
+    </div>
+    <footer>
+        <p id="FooterText">
+            Kantonsschule Büelrain
+            RosenStrasse 1 
+            8400 Winterthur 
+            Copyright ©-All rights are reserved
+        </p>
+    </footer>
+
 </body>
 
 </html>
