e649cc3ae100826643cef0b54dd36d769bc97cc2
EricStautmeister <71490551+EricStautmeister@users.noreply.github.com>
2023-03-22 10:49:15 +0100

    hero section done

--- view/home.php
+++ view/home.php
@@ -8,6 +8,7 @@
     <title>Real Nutrition</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="view/styles/index.css">
     <link rel="stylesheet" href="view/styles/home.css">
     <link rel="stylesheet" href="view/styles/normalise.css">
     <script src="" async defer></script>
@@ -18,16 +19,43 @@
     <header>
         <form action="" method="post"></form>
     </header>
-    
-    <section id="hero-section">
-        <div id="hero-text">
+
+    <div class="hero hero-section">
+        <div class="hero-text-wrapper">
             <h1>Real Nutrition</h1>
             <p>Real Nutrition is a website that helps you to keep track of your nutrition. You can add your own food and meals and keep track of your daily nutrition.</p>
-            <a href="view/login.php">Login</a>
-            <a href="view/register.php">Register</a>
+            <button>
+                <a href="view/login.php">Login</a>
+            </button>
         </div>
-    </section>
-    
+    </div>
+
+    <div class="content">
+        <div class="function-preview">
+            <button>
+                Text
+            </button>
+            <button>
+                Text2
+            </button>
+            <button>
+                Text3
+            </button>
+        </div>
+        <div class="content-section">
+            <h2>What is Real Nutrition?</h2>
+            <p>Real Nutrition is a website that helps you to keep track of your nutrition. You can add your own food and meals and keep track of your daily nutrition.</p>
+        </div>
+        <div class="content-section">
+            <h2>How does it work?</h2>
+            <p>Real Nutrition is a website that helps you to keep track of your nutrition. You can add your own food and meals and keep track of your daily nutrition.</p>
+        </div>
+        <div class="content-section">
+            <h2>Why should I use it?</h2>
+            <p>Real Nutrition is a website that helps you to keep track of your nutrition. You can add your own food and meals and keep track of your daily nutrition.</p>
+        </div>
+    </div>
+
     <footer>
         <p id="FooterText">
             Kantonsschule Büelrain
