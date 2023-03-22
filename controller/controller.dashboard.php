31e33a1d288c9e6b5197de5b4ba0e88c69344693
MerlinMinder <merlin.coding@gmail.com>
2023-03-22 16:01:43 +0100

    fixed mail

--- controller/controller.dashboard.php
+++ controller/controller.dashboard.php
@@ -5,8 +5,9 @@ class DashboardController {
         include "./view/dash.php";
     }
 
-    public function loginUser($newuser) {
+    public function loginUser($newuser, $uid) {
         session_start();
         $_SESSION["user"] = $newuser;
+        $_SESSION["uid"] = $uid;
     }
 }
