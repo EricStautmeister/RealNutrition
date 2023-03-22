1898b29e3091567c7c9ddc4cd73620eff573d38f
EricStautmeister <71490551+EricStautmeister@users.noreply.github.com>
2023-03-22 15:11:00 +0100

    dash temp

---
+++ view/dash.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>
+        Real Nutrition | Dashboard
+    </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="view/styles/index.css">
+    <link rel="stylesheet" href="view/styles/normalise.css">
+</head>
+
+<body>
+
+    <?php
+    
+    ?>
+
+</body>
+
+<footer>
+    Kantonsschule Büelrain
+    RosenStrasse 1
+    8400 Winterthur
+    Copyright ©-All rights are reserved
+</footer>
+
+</html>
