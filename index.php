9075ac8512c71894413eab21db4ca1fbaddfbdea
MerlinMinder <merlin.coding@gmail.com>
2023-03-23 10:48:31 +0100

    styling

--- index.php
+++ index.php
@@ -4,21 +4,20 @@ ini_set('display_errors', 0);
 require_once './vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
-require_once './model/model.php';
+
 require_once "./controller/controller.home.php";
 require_once "./controller/controller.auth.php";
 require_once "./controller/controller.dashboard.php";
 require_once "./controller/controller.email.php";
 
-
 require "./vendor/phpmailer/phpmailer/src/PHPMailer.php";
 require "./vendor/phpmailer/phpmailer/src/SMTP.php";
 require "./vendor/phpmailer/phpmailer/src/Exception.php";
 
-
-require_once "./controller/controller.dashboard.php";
+require_once './model/model.php';
 require_once "./model/auth.model.php";
 require_once "./model/food.model.php";
+require_once "./model/user.model.php";
 
 try {
 
