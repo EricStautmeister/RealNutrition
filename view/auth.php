267306fc331db1bb7749223abbcacd314f454a26
MerlinMinder <merlin.coding@gmail.com>
2023-03-28 09:56:51 +0200

    meeeer

--- view/auth.php
+++ view/auth.php
@@ -20,7 +20,7 @@
 <body>
     <div class="overlay-container">
         <nav class="navbar">
-            <p class="user-name"><?php echo ucfirst($_SESSION["user"]) ?></p>
+            <p class="user-name"></p>
 
             <div class="navbar-container container">
                 <input type="checkbox" name="" id="">
@@ -33,12 +33,7 @@
                     <li><a onclick="location.href='/home'">Home</a></li>
                     <li><a onclick="location.href='/dashboard'">Dashboard</a></li>
                     <!-- <li><a onclick="location.href='/about-us'">About Us</a></li> -->
-                    <?php if (isset($_SESSION["user"])) {
-                        echo "<li><a onclick=\"location.href='/logout'\">Logout</a></li>";
-                    } else {
-                        echo "<li><a onclick=\"location.href='/login'\">Login</a></li>";
-                        echo "<li><a onclick=\"location.href='/signup'\">Signup</a></li>";
-                    } ?>
+
                 </ul>
             </div>
         </nav>
